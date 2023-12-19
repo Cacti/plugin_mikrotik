a5f33615f03884b9309ef002e1e0d674cee1d567
Piotr Górski <piotr@prokocim.org>
2023-12-19 16:43:18 +0100

    Fixed errors (#77)

--- poller_mikrotik.php
+++ poller_mikrotik.php
@@ -781,7 +781,7 @@ function checkHost($host_id) {
 			// Remove old records
 			db_execute_prepared('DELETE FROM plugin_mikrotik_users
 				WHERE userType=0
-				AND name RLIKE '" . read_config_option('mikrotik_user_exclusion') . "'
+				AND name RLIKE ' . read_config_option('mikrotik_user_exclusion') . '
 				AND present = 0
 				AND host_id = ?
 				AND last_seen < FROM_UNIXTIME(UNIX_TIMESTAMP()-' . read_config_option('mikrotik_user_exclusion_ttl') . ')',
