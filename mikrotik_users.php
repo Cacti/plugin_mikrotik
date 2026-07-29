5d881c4d29a06abe99ea71b98e418246e637c0c3
Thomas Vincent <thomasvincent@gmail.com>
2026-07-28 20:38:09 -0700

    fix(security): pass allowed_classes to unserialize, not get_request_var
    get_request_var()'s 2nd argument is a default value, not options for
    unserialize(). Passing array('allowed_classes' => false) there made
    stripslashes() receive an array and left unserialize() running with
    no class restriction, reintroducing PHP object injection risk. Also
    guard against a non-array unserialize() result.
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- mikrotik_users.php
+++ mikrotik_users.php
@@ -60,7 +60,11 @@ function form_actions() {
 
 	/* if we are to save this form, instead of display it */
 	if (isset_request_var('selected_items')) {
-		$selected_items = unserialize(stripslashes(get_request_var('selected_items', array('allowed_classes' => false))));
+		$selected_items = unserialize(stripslashes(get_request_var('selected_items')), array('allowed_classes' => false));
+
+		if (!is_array($selected_items)) {
+			$selected_items = array();
+		}
 
 		if (get_request_var('drp_action') == '1') { /* delete */
 			if (!isset_request_var('delete_type')) { set_request_var('delete_type', 2); }
