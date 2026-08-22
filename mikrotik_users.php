4ec4168a41a6e0e46d7c772a98c697dc34cad5f0
Thomas Vincent <thomasvincent@gmail.com>
2026-08-22 02:45:29 -0700

    fix: address review findings
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- mikrotik_users.php
+++ mikrotik_users.php
@@ -60,7 +60,10 @@ function form_actions() {
 
 	/* if we are to save this form, instead of display it */
 	if (isset_request_var('selected_items')) {
-		$selected_items = unserialize(stripslashes(get_request_var('selected_items')), array('allowed_classes' => false));
+		$serialized_items = get_request_var('selected_items');
+		$selected_items = is_string($serialized_items)
+			? @unserialize(stripslashes($serialized_items), array('allowed_classes' => false))
+			: false;
 
 		if (!is_array($selected_items)) {
 			$selected_items = array();
